cc31db2c07ecedb96c1f84581fa8558c9e088bf4
spascual90 <spascual90@gmail.com>
2026-02-23 09:56:49 +0500

    Remove bundled Arduino libraries; update .sproject
    Delete multiple bundled Arduino libraries from libraries/ (notably Adafruit_BNO055 and its utilities, Adafruit_Unified_Sensor, NeoGPS, NeoHWSerial, LinearRegression, br3ttb PID, simplot, virtuinoCM, and others) to clean up the repo. Update .sproject to change the board from 'unor4wifi' to 'mega' and point to the AVR boards.txt; adjust the CPU menu message. Also includes a modification to sloeber.ino.cpp.

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2025-09-27 15:24:08
+//This file has been generated on 2026-02-23 09:48:14
 
 #include "Arduino.h"
 #include <Arduino.h>
